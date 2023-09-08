1bd5239ad6cc435496bca1b52f8ba340df823868
shubham-singh-31 <shubham.s@antiersolutions.com>
2023-09-08 10:44:19 +0530

    code for testing

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -6,6 +6,7 @@ export const validateEmail = (email) => {
         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
       );
   };
+  
   export const debouncing = (func) => {
     let timer;
     return function (...args) {
